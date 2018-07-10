5d695f2571cddcff469b0c2a1d45ae4a8a8ff9db
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2018-07-10 08:10:00 -0500

    Order wallet by first name

--- cryptocoin/user/views/view_account.py
+++ cryptocoin/user/views/view_account.py
@@ -35,7 +35,7 @@ def get_context(request, get_all_users):
     context = {}
     userdata = get_object_or_404(UserData, username=request.user.username)
     if get_all_users:
-        allusers = UserData.objects.filter(school=userdata.school).values('username', 'first_name', 'last_name', 'is_admin')
+        allusers = UserData.objects.filter(school=userdata.school).values('username', 'first_name', 'last_name', 'is_admin').order_by('first_name')
         context['allusers'] = allusers
     try:
         useranswers = get_object_or_404(UserAnswers, data=userdata)
